8c75e08f05cd84d7aa2df51295eaa6ccdef8fd6a
nccvector <fzn1020@gmail.com>
2024-02-24 20:39:21 +0900

    update action

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1708774581',resources: [
+    cacheName: 'chirpy-1708774753',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       

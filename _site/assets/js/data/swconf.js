c23eed11d40e0de94a04eda4cf69f8bb76a8e023
nccvector <fzn1020@gmail.com>
2024-02-24 20:46:05 +0900

    remove python gen posts action

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1708775122',resources: [
+    cacheName: 'chirpy-1708775155',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       

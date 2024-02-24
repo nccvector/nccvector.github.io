6e5700e6c8bb5c54eb14389e3902390b6c7154d4
nccvector <fzn1020@gmail.com>
2024-02-24 20:34:38 +0900

    change deploy action for posts

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1708774323',resources: [
+    cacheName: 'chirpy-1708774462',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       

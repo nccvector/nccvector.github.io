c53728d65e329f1c5322e9090ad6ddeefb392ee2
nccvector <fzn1020@gmail.com>
2024-02-24 20:25:39 +0900

    add python postgenerator action

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1708773089',resources: [
+    cacheName: 'chirpy-1708773918',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       

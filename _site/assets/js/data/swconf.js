f51586471d0aeb4334d3ff8c35e2dd757ccc9de6
nccvector <fzn1020@gmail.com>
2024-02-24 20:44:13 +0900

    removed submodule

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1708774753',resources: [
+    cacheName: 'chirpy-1708775023',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       

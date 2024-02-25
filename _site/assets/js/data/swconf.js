e751754cc027ab81fb140634e1c4338de60dd006
nccvector <fzn1020@gmail.com>
2024-02-25 11:00:05 +0900

    favicon

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1708825274',resources: [
+    cacheName: 'chirpy-1708826374',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
